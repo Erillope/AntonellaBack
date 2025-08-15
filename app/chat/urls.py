a334ef5b0fb09276f526d189c881c2d8126f5d97
erillope <ericklorenzolopez1@gmail.com>
2025-08-15 02:17:03 -0500

    endpoints para info mensajes

--- app/chat/urls.py
+++ app/chat/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import ChatApiView, UserChatView, AdminChatView
+from .views import ChatApiView, UserChatView, AdminChatView, AdminReadChatView, AdminReadChatMessageView
 
 urlpatterns = [
     path('', ChatApiView.as_view()),
     path('from_user/', UserChatView.as_view()),
     path('from_admin/', AdminChatView.as_view()),
+    path('admin/read/', AdminReadChatView.as_view()),
+    path('admin/read/message/', AdminReadChatMessageView.as_view()),
 ]
