ce282775ab53d716f82dcc4f3d975b7ed73837ed
erillope <ericklorenzolopez1@gmail.com>
2025-08-17 16:58:55 -0500

    fix serializer errors

--- app/order/urls.py
+++ app/order/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import (OrderApiView, ServiceItemApiView, ProductItemApiView, ServiceItemFilterApiView, 
-                    EmployeeServiceInfoView, FilterOrderApiView, EmployeePaymentFilterSerializer, EmployeePaymentSerializer)
+                    EmployeeServiceInfoView, FilterOrderApiView, EmployeePaymentFilterView, EmployeePaymentView)
 
 urlpatterns = [
     path('', OrderApiView.as_view()),
@@ -9,6 +9,6 @@ urlpatterns = [
     path('service-item/filter/', ServiceItemFilterApiView.as_view()),
     path('service-item/employee-info/', EmployeeServiceInfoView.as_view()),
     path('filter/', FilterOrderApiView.as_view()),
-    path('employee-payment/filter/', EmployeePaymentFilterSerializer.as_view()),
-    path('employee-payment/', EmployeePaymentSerializer.as_view()),
+    path('employee-payment/filter/', EmployeePaymentFilterView.as_view()),
+    path('employee-payment/', EmployeePaymentView.as_view()),
 ]
